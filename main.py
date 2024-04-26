c466feaa50717683a787c48da5b00004f4c05f5b
gcancel <33400121+gcancel@users.noreply.github.com>
2024-04-25 23:03:57 -0500

    saving changes for main.py

--- main.py
+++ main.py
@@ -1,61 +1,68 @@
-#Wrapping the read logic into a function
+#Bookbot App
+#Functionality: word Count, letter count, sorted character count report
 
+#Main function
 def main():
+    #declaring our variables for use:
+    book_file = './books/frankenstein.txt'
+    book = get_text(book_file)
+    word_count = num_words(book)
+    total_letters = count_char(book)
+    sorted_letter_list = sorted_dic_letters(total_letters)
+
+    #printing outputs
+    #word count
+    print(f'--- Printing Outputs ---\n Total amount of words in book: {word_count}')
+
+    #letter count
+    print('Now counting letters in the book.')
+    print(f'Data: {total_letters}')
+    print('Sorting data to make it more presentable...')
+    print('--- Printing Report ---')
+
+    #now for the sorted report
+    for item in sorted_letter_list:
+        #isaplha is used to return only alphabet letters
+        if not item["letter"].isalpha():
+            continue
+        print(f'The {item["letter"]} character was found {item["num"]} times!')
+
+    print('--- Report Complete ---')
+
+#pulling text from file
+def get_text(text):
     try:
-        with open('./books/frankenstein.txt', 'r') as f:
-            data = f.read()
-            print(data)
-        return data
+        with open(text, 'r') as f:
+            return f.read()
     except FileNotFoundError as e:
-        print(e, "Sorry there seems to be an issue with this file...")
+        print(e, "oopsies")
 
-def count_words():
-    with open('./books/frankenstein.txt', 'r') as f:
-        data = f.read()
-        words = data.split()
-        return len(words)
+#getting total words
+def num_words(b):
+    words = b.split()
+    return len(words)
 
-def count_letters():
+#counting the characters
+def count_char(b):
+    letters = {}
+    for letter in b:
+        l_letter = letter.lower()
+        if l_letter in letters:
+            letters[l_letter] += 1
+        else:
+            letters[l_letter] = 1
+    return letters
 
-    try:
-        with open('./books/frankenstein.txt', 'r') as f:
-            letters = {}
-            data = f.read()
-            for c in data:
-                new_data = c.lower()
-                if new_data in letters:
-                    letters[new_data] += 1
-                else:
-                    letters[new_data] = 1
-            return letters
-    except Exception as e:
-        print(e, "oopsies!!")
+#function used with the sort method
+def sort_on(d):
+    return d["num"]
 
-report_data = count_letters()
-def sort_on(report_data):
-    return report_data["num"]
+#cconverting the dictionary into a list
+def sorted_dic_letters(d):
+    n_list = []
+    for item in d:
+        n_list.append({"letter": item, "num": d[item]})
+    n_list.sort(reverse=True, key=sort_on)
+    return n_list
 
-def dict_to_list_sorted(r):
-    sorted = []
-    for c in r:
-        sorted.append({"char": c, "num": r[c]})
-    sorted.sort(reverse=True, key=sort_on)
-    return sorted
-
-def report(r):
-    for letter in r:
-        c = r[letter]
-        print(f'The {letter} character was found {c} times')
-    print('--- End report ---')
-
-
-
-
-
-
-#Run the main function
-#main()
-print(count_words())
-print(count_letters())
-""" report(report_data) """
-print(dict_to_list_sorted(report_data))
+main()
